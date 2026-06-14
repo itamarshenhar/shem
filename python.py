d2b47ce61aac03e939bc6fca3fdf60b45464e9fe
Itamar <itamar.shenhar@mail.huji.ac.il>
2026-06-14 13:36:55 +0300

    update

---
+++ python.py
@@ -0,0 +1,7 @@
+# write exmple code in python
+# This is an example of a simple Python program that prints "Hello, World!" to the console.
+def main():
+    print("Hello, World!")
+if __name__ == "__main__":
+    main()
+    
